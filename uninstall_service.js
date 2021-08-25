21f5be87d2e7ed76567c40302e614cbe03bd9fd4
Felipe Santos <felipesantosswengineer@gmail.com>
2021-08-25 07:20:22 -0300

    install as service

---
+++ uninstall_service.js
@@ -0,0 +1,16 @@
+var Service = require('node-windows').Service;
+// Criando um novo objeto do Serviço
+var svc = new Service({
+//Nome do servico
+name:'server-ldc',
+//Descricao que vai aparecer no Gerenciamento de serviço do Windows
+description: 'Servidor do sistema LDC',
+//caminho absoluto do seu script
+//script: 'C:\\tutorial\\app.js'
+script: 'C:\\Users\\felipe\\projetos\\javascript\\projeto-ldc\\server-ldc\\index.js'
+});
+svc.on('uninstall',function(){
+console.log('Uninstall complete.');
+});
+// Desistalar serviço.
+svc.uninstall();
